9e1753ddbf5311b98faee31f64e67d2ee56238a7
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-02-04 20:31:55 +0100

    design refresh, start and setting button in Timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,3 +1,6 @@
+import refresh from '../assets/reload.png';
+import setting from '../assets/gear.png'
+
 export default function Timer(){
   return (
     <div className="">
@@ -6,9 +9,10 @@ export default function Timer(){
         <p>minutes</p>
       </div>
       <div className="flex justify-center ">
-        <button>refresh</button>
-        <button>start</button>
-        <button>setting</button>
+        <button className='rounded-xl bg-zinc-700 shadow-xs shadow-blue-400/50 '><img src={refresh} className="h-5 w-5 m-2"/></button>
+        <button className="font-mono border rounded-full px-6 w-fit text-white bg-blue-400 border-cyan-400 backdrop-blur-md mx-4 shadow-md shadow-blue-400/50
+      ">start</button>
+        <button className='rounded-xl bg-zinc-700 shadow-xs shadow-blue-400/50 '><img src={setting} className="h-5 w-5 m-2"/></button>
       </div>
       <div className="flex justify-center ">
         <button>25 min</button>
