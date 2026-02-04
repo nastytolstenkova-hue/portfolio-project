ccff0a2443689771823cf5bc58feba280396ced9
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-02-04 21:08:46 +0100

    designing timer component: bottom part + circle for timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,22 +2,28 @@ import refresh from '../assets/reload.png';
 import setting from '../assets/gear.png'
 
 export default function Timer(){
+  const designButtonTime = 'font-mono text-sm rounded-full px-4 py-1 w-fit text-white bg-zinc-700 mx-4 shadow-xs shadow-blue-400/50 '
+  const activeButton = ' active:cursor-pointer active:shadow-none  hover:cursor-pointer hover:shadow-md'
   return (
     <div className="">
-      <div className="flex justify-center ">
-        <p>time</p>
-        <p>minutes</p>
+      <div className="flex justify-center my-5">
+        <div className='border p-19 rounded-full'>
+          <p>time</p>
+          <p>minutes</p>
+
+        </div>
+        
       </div>
       <div className="flex justify-center ">
-        <button className='rounded-xl bg-zinc-700 shadow-xs shadow-blue-400/50 '><img src={refresh} className="h-5 w-5 m-2"/></button>
-        <button className="font-mono border rounded-full px-6 w-fit text-white bg-blue-400 border-cyan-400 backdrop-blur-md mx-4 shadow-md shadow-blue-400/50
-      ">start</button>
-        <button className='rounded-xl bg-zinc-700 shadow-xs shadow-blue-400/50 '><img src={setting} className="h-5 w-5 m-2"/></button>
+        <button className={`rounded-xl bg-zinc-700 shadow-xs shadow-blue-400/50 ${activeButton} `}><img src={refresh} className="h-5 w-5 m-2"/></button>
+        <button className={`font-mono border rounded-full px-6 w-fit text-white bg-blue-400 border-cyan-400 backdrop-blur-md mx-4 shadow-md shadow-blue-400/50 ${activeButton}
+      `}>start</button>
+        <button className={`rounded-xl bg-zinc-700 shadow-xs shadow-blue-400/50 ${activeButton} `}><img src={setting} className="h-5 w-5 m-2"/></button>
       </div>
-      <div className="flex justify-center ">
-        <button>25 min</button>
-        <button>45 min</button>
-        <button>60 min</button>
+      <div className="flex justify-center my-7">
+        <button className={`${designButtonTime} ${activeButton}`}>25 min</button>
+        <button className={`${designButtonTime} ${activeButton}`}>45 min</button>
+        <button className={`${designButtonTime} ${activeButton}`}>60 min</button>
       </div>
     </div>
   )
--- src/pages/FocusTimer.tsx
+++ src/pages/FocusTimer.tsx
@@ -3,7 +3,7 @@ import Timer from "../components/Timer"
 export default function FocusTimer(){
   return (
     <div>
-      <h2 className="font-mono border rounded-2xl p-1 px-6 w-fit mx-auto  text-cyan-300 bg-blue-500/10 border-blue-500/30 backdrop-blur-md 
+      <h2 className="font-mono border rounded-2xl p-1 px-6 w-fit mx-auto mb-8  text-cyan-300 bg-blue-500/10 border-blue-500/30 backdrop-blur-md  
       ">Focus Session</h2>
       <Timer/>
     </div>
