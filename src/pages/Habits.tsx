8042d7dfede4a1a5edb6591303285166144bfd88
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-02-08 20:13:52 +0100

    added design for habits page

--- src/pages/Habits.tsx
+++ src/pages/Habits.tsx
@@ -15,8 +15,9 @@ export default function Habits(){
   return (
     <div>
       <div>
-        <h1>My habits</h1>
-        <button onClick={openModal}>+ Add</button>
+        <h1  className="font-mono border rounded-2xl p-1 px-6 w-fit mx-auto mb-8  text-cyan-300 bg-blue-500/10 border-blue-500/30 backdrop-blur-md    
+      ">My habits</h1>
+        <button onClick={openModal} className="font-mono border rounded-full px-6 h-9 w-fit text-white  backdrop-blur-md mx-4 shadow-md shadow-blue-400/50 bg-blue-400 border-cyan-400 active:cursor-pointer active:shadow-none active:scale-95 hover:cursor-pointer hover:shadow-md">+ Add</button>
       </div>
       {isModal && <FormAddHabit closeFunction={closeModal}/>}
       
