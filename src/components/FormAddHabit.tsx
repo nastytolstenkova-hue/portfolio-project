b59f94b20bc726659a86afa4e3f8a765830a9996
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-02-08 20:07:39 +0100

    added close + open modal function. Input value.

--- src/components/FormAddHabit.tsx
+++ src/components/FormAddHabit.tsx
@@ -2,7 +2,11 @@ import { useState } from "react"
 
 const inputDesign = " p-2 border border-blue-500 rounded-md focus:ring-2 focus:ring-blue-700 outline-none m-2 w-90 text-blue-100"
 
-export default function FormAddHabit(){
+interface FormAddHab {
+  closeFunction:() => void;
+}
+
+export default function FormAddHabit({closeFunction}:FormAddHab){
   const[habitName, setHabitName] = useState<string>('');
   const[description, setDescription] = useState<string>('');
 
@@ -12,11 +16,11 @@ export default function FormAddHabit(){
         
         <h1 className="text-blue-400 m-2">Add new habit</h1>
         
-        <button type='button' className="text-blue-300 rounded-xs w-6 h-6 ml-7.5 absolute top-2 right-4  active:cursor-pointer hover:cursor-pointer ">✕</button>
+        <button type='button' className="text-blue-300 rounded-xs w-6 h-6 ml-7.5 absolute top-2 right-4  active:cursor-pointer hover:cursor-pointer " onClick={closeFunction}>✕</button>
         <div className="flex flex-col mt-7">
           
-          <input className={inputDesign} placeholder="habit"/>
-          <input className={inputDesign} placeholder="mini-description"/>
+          <input className={inputDesign} placeholder="habit" value={habitName} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setHabitName(e.target.value)}/>
+          <input className={inputDesign} placeholder="mini-description" value={description} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setDescription(e.target.value)}/>
           <div className="flex justify-end">
             <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-8 my-3 rounded-full shadow-[0_0_20px_rgba(59,130,246,0.4)] active:scale-95 transition-all cursor-pointer">Add</button>
 
--- src/pages/Habits.tsx
+++ src/pages/Habits.tsx
@@ -1,15 +1,25 @@
-
+import { useState } from "react";
 import FormAddHabit from "../components/FormAddHabit"
 
 export default function Habits(){
+  const [isModal, setIsModal] = useState<boolean>(false)
+
+  const openModal = () => {
+    setIsModal(true);
+  }
+
+  const closeModal = () => {
+    setIsModal(false);
+  }
 
   return (
     <div>
       <div>
         <h1>My habits</h1>
-        <button>+ Add</button>
+        <button onClick={openModal}>+ Add</button>
       </div>
-      <FormAddHabit/>
+      {isModal && <FormAddHabit closeFunction={closeModal}/>}
+      
     </div>
   )
 }
