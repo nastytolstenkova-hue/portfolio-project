865a37af868e66b23479d2fc00544b716778fc3d
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-02-15 21:38:02 +0100

    Created statictic's structure

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+
 export interface OneHab {
       id: string;
       habitName: string;
--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,7 +1,29 @@
 export default function Statistics(){
   return (
     <div>
-      <p>statistics</p>
+      <h1>Space Analytics</h1>
+      <div>
+        <div>
+          <p>Total number of habits</p>
+          <p>7</p>
+        </div>
+        <div>
+          <p>Challenges won</p>
+          <p>2</p>
+        </div>
+        <div>
+          <p>Efficiency</p>
+          <p>7</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>Habits in the progress</p>
+        </div>
+        <div>
+          <p>User's strike</p>
+        </div>
+      </div>
     </div>
   )
 }
