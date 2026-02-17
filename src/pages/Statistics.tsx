0e10637692e9e9c5e8506bbe6471d648bcf2216e
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-02-17 20:57:16 +0100

    Made refinement on Statistics page.

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -9,10 +9,31 @@ export default function Statistics(){
     }
   
     const { habitsList } = context;
+
+    const totalDone = habitsList.reduce((acc, habit) => 
+      acc + habit.completedDays.filter(Boolean).length, 0);
+
+
+    const totalPossible = habitsList.length * 21;
+
+
+    const efficiency = totalPossible > 0 
+      ? Math.round((totalDone / totalPossible) * 100) 
+      : 0;
+
+    const bestStreak = habitsList.reduce((max, habit) => {
+  
+      let currentStreak = 0;
+      for (let day of habit.completedDays) {
+        if (day) currentStreak++;
+        else break; 
+  }
+    return Math.max(max, currentStreak);
+    }, 0);
     
   return (
-    <div className="">
-      <h1 className="font-mono border rounded-2xl p-1 px-6 w-fit mx-auto mb-8  text-cyan-300 bg-blue-500/10 border-blue-500/30 backdrop-blur-md  
+    <div className="font-mono">
+      <h1 className=" border rounded-2xl p-1 px-6 w-fit mx-auto mb-8  text-cyan-300 bg-blue-500/10 border-blue-500/30 backdrop-blur-md  
       ">Space Analytics</h1>
       <div className="grid grid-cols-3 font-mono gap-3">
         <div className="border border-cyan-400 rounded-lg p-5 shadow-[inset_0_0_25px_rgba(59,130,246,0.4)] bg-cyan-900/10 grid justify-center">
@@ -25,18 +46,18 @@ export default function Statistics(){
         </div>
         <div className="border border-green-400 rounded-lg p-5 shadow-[inset_0_0_25px_rgba(59,130,246,0.4)] bg-green-900/10 grid justify-center  ">
           <p className="flex justify-center text-sm text-green-600">Efficiency</p>
-          <p className="flex justify-center text-5xl text-green-500">7</p>
+          <p className="flex justify-center text-5xl text-green-500">{efficiency}%</p>
         </div>
       </div>
       <div className="mt-6 border border-zinc-400 rounded-lg p-5 grid grid-cols-2">
         <div>
-          <p>Habits in the progress</p>
-          <div>
+          <p className="flex justify-center mb-2">Habits in the progress</p>
+          <div className="max-h-48 border border-yellow-200 p-4 rounded-lg overflow-y-auto">
             {habitsList.map((habit)=>
                           habit.completedDays.filter(Boolean).length !== 21 && 
                           <div className="grid grid-cols-2">
                             <p>{habit.habitName}</p>
-                            <p>
+                            <p className="flex justify-end">
                               {
                     Math.round(((habit.completedDays.filter(Boolean).length)/21)*100)} %
                             </p>
@@ -44,8 +65,14 @@ export default function Statistics(){
                         )}
                       </div>
           </div>
-        <div>
-          <p className="flex justify-end">User's strike</p>
+        <div className="grid">
+          <p className="flex justify-end mr-5">User's strike</p>
+          <div className="text-yellow-400 ">
+            <p className="flex p-2 ml-10 justify-end text-5xl rounded-full shadow-[inset_0_0_25px_rgba(59,130,246,0.4)]">{bestStreak}</p>
+            <p className="flex justify-end text-xm mr-5">Days</p>
+
+          </div>
+          
         </div>
       </div>
     </div>
