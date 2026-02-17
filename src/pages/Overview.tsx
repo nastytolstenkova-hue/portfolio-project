43808dfc6336bfe5b1451aee3b6ea3c645bff5e7
Anastasia Tolstenkova <nastytolstenkova@gmail.com>
2026-02-17 21:18:37 +0100

    Created structure for Overview component

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,7 +1,18 @@
 export default function Overview(){
   return (
-    <div>
-      <p>overview</p>
+    <div className="font-mono">
+      <p className=" border rounded-2xl p-1 px-6 w-fit  mb-8  text-cyan-300 bg-blue-500/10 border-blue-500/30 backdrop-blur-md  
+      ">Overview</p>
+      <h1 className="text-4xl text-cyan-400 mb-4">Welcome back, Captain!</h1>
+      <p className="text-sm mb-7">System status: All systems go. 💫</p>
+      <div className='grid grid-cols-2 gap-5'>
+        <div className="border border-cyan-500">
+          <p className="flex justify-center">Todays Focus</p>
+        </div>
+        <div className="border border-violet-500">
+          <p className="flex justify-center">Quick Focus</p>
+        </div>
+      </div>
     </div>
   )
 }
